d9afe44875ffa2a6d6e58a36399f11a97664bce5
Harshali Patil <harshali.patil@mindbowser.com>
2026-05-10 12:52:19 +0530

    "feat:Added api to conncet to backend"

--- src/features/shared/api/staffApi.ts
+++ src/features/shared/api/staffApi.ts
@@ -7,3 +7,9 @@ export const getStaffMembersApi = (params: {
 }) => {
   return API.get("/staff/members", { params })
 }
+export const getStaffMemberDashboardApi = (
+  memberId: number,
+  params: { page: number; limit: number }
+) => {
+  return API.get(`/staff/members/${memberId}/dashboard`, { params })
+}
